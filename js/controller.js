0d8464bb7356d1d2dd9616ec80d1a24f2ceca7b7
wzuniga <wzuniga@episunsa.edu.pe>
2016-07-20 21:06:24 -0500

    Adding progress bar

--- js/controller.js
+++ js/controller.js
@@ -1,12 +1,14 @@
 var app = angular.module('myApp', []);
 
-app.controller('myCtrl', function($scope, $http) {
+app.controller('myCtrl', function($scope, $http, $interval) {
     
     $scope.songs = [];
     $scope.test = "";
     $scope.volumen = 7;
 
     $scope.varInit = 0;
+    $scope.percent = 0;
+    $scope.state = true;
 
     /*var arr = [{grupo : "Mana", name:"Mariposa Traicionera"},
                {grupo : "Mana", name:"Mi verdad"},
@@ -34,20 +36,21 @@ app.controller('myCtrl', function($scope, $http) {
     $http.post("php/obtenerListaF.php", {
     }).success(function(respuesta){
         $scope.songs = respuesta;
-            console.log(respuesta);
+        //console.log(respuesta);
     });
 
     $http.post("php/obtenerCanciones.php", {
     }).success(function(respuesta){
         $scope.n_songs = respuesta;
-            console.log(respuesta);
+            //console.log(respuesta);
+            $scope.songPlaying(1);
     });
 
     //captar la cancion
     $scope.songPlaying = function(id){
         
         if($scope.varInit == 0){
-            console.log("nullooo");
+            //console.log("nullooo");
             $scope.varInit = 1;
         }else{
 
@@ -55,8 +58,8 @@ app.controller('myCtrl', function($scope, $http) {
             //console.log("Hey mama");
             /*$http.post("php/llenarDatos.php", {
                 id: $scope.currentSong_old.id,
-                volumen: "5",
-                porcentaje: "90"
+                volumen: $scope.volumen,
+                porcentaje: $scope.percent
             }).success(function(respuesta){
                 console.log(respuesta);
             });*/
@@ -71,9 +74,17 @@ app.controller('myCtrl', function($scope, $http) {
 
     $scope.play = function(){
         
-        console.log($scope.n_songs.length);
-        $scope.songPlaying(1);
-        console.log($scope.currentSong.id);
+        //console.log($scope.n_songs.length);
+        //$scope.songPlaying(1);
+        //console.log($scope.currentSong.id);
+        //console.log(angular.isDefined($scope.timer));
+        if ($scope.state) {
+            $interval.cancel($scope.timer);
+            $scope.state = false;
+        }else{
+            $scope.timer = $interval( function(){ $scope.callAtInterval(); }, 1000);
+            $scope.state = true;
+        }
     }    
     
     $scope.next = function(){
@@ -83,7 +94,8 @@ app.controller('myCtrl', function($scope, $http) {
         }else{
             var song_id = parseInt($scope.currentSong.id)%$scope.n_songs.length;
             $scope.songPlaying(song_id+1);
-            console.log(parseInt($scope.currentSong.id));
+            $scope.percent = 0;
+            //console.log(parseInt($scope.currentSong.id));
         }
     }  
 
@@ -95,7 +107,8 @@ app.controller('myCtrl', function($scope, $http) {
             var id_current = parseInt($scope.currentSong.id);
             var song_id = (id_current==1)?$scope.n_songs.length+1:id_current;
             $scope.songPlaying(song_id-1);
-            console.log(parseInt($scope.currentSong.id));
+            $scope.percent = 0;
+            //console.log(parseInt($scope.currentSong.id));
         }
     }
 
@@ -106,4 +119,15 @@ app.controller('myCtrl', function($scope, $http) {
             $scope.volumen = 0;
     }
 
+    $scope.timer = $interval( function(){ $scope.callAtInterval(); }, 1000);
+    
+    $scope.callAtInterval = function() {
+        if ( $scope.percent > 100 ){
+            $scope.next();
+            $scope.percent = 0;
+        }
+        $scope.percent++;
+        //console.log("$scope.callAtInterval - Interval occurred");
+    }
+    
 });
